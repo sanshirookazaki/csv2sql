dfada7821a0ede528dc2ef688ad178aa00358833
sanshirookazaki <sanshirookazaki@gmail.com>
2019-10-31 01:15:35 +0900

    add ignore error message

--- db/tx.go
+++ db/tx.go
@@ -1,11 +1,17 @@
 package db
 
-import "github.com/shogo82148/txmanager"
+import (
+	"github.com/shogo82148/txmanager"
+
+	"github.com/fatih/color"
+)
 
 func TxExecQuery(dbm txmanager.DB, query string, force bool) error {
 	err := txmanager.Do(dbm, func(tx txmanager.Tx) error {
 		_, err := tx.Query(query)
-		if force {
+		if force && err != nil {
+			fy := color.New(color.FgYellow)
+			fy.Println("Ignore :", err)
 			return nil
 		}
 
